f848f95923a7c7ac532b9b86adf886235c5e1dca
ajpadilla88 <apadilla@theegg.io>
2020-11-26 10:45:14 -0400

    First commit

---
+++ apps/mooc/backend/src/Controller/HealthCheck/HealthCheckGetController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodelyTv\Apps\Mooc\Backend\Controller\HealthCheck;
+
+use CodelyTv\Shared\Domain\RandomNumberGenerator;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+final class HealthCheckGetController
+{
+
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'mooc-backend' => 'ok'
+            ]
+        );
+    }
+}
---
+++ src/Shared/Domain/RandomNumberGenerator.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace CodelyTv\Shared\Domain;
+
+interface RandomNumberGenerator
+{
+
+}
